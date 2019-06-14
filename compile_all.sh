c28e34c9e3d6281bc2d4cac725848f12ac6b1e3f
Pascal <pascal.lu@icm-institute.org>
2019-06-14 21:29:22 +0200

    add html

--- compile_all.sh
+++ compile_all.sh
@@ -1,4 +1,36 @@
-cd document/pve/counters/
+cd document/pve/
+
+cd counters/
 xelatex Counters.tex
-pdf2htmlex Counters.pdf
-cd ../../..
+pdf2htmlex --zoom 2 Counters.pdf
+cd ..
+
+cd gen4_evolution/
+xelatex gen_4.tex
+pdf2htmlex --zoom 2 gen_4.pdf
+cd ..
+
+cd guide/
+xelatex Guide.tex
+pdf2htmlex --zoom 2 Guide.pdf
+cd ..
+
+cd gym/
+xelatex Gym.tex
+pdf2htmlex --zoom 2 Gym.pdf
+cd ..
+
+cd legendary_raid/
+xelatex Raid_bosses.tex
+pdf2htmlex --zoom 2 Raid_bosses.pdf
+cd ..
+
+cd iv_determined_at_go/
+xelatex iv_determined_at_go.tex
+pdf2htmlex --zoom 2 iv_determined_at_go.pdf
+cd ../..
+
+cd ../pvp/counters
+xelatex pvp.tex
+pdf2htmlex --zoom 2 pvp.pdf
+cd ../..
